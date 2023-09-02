3d683309024e5c9d71e3a7ba02048a52090949e2
Ostapenko <abc@mail.ru>
2023-09-02 21:40:28 +0300

    Feat: added creation and deleting of new keys and values

--- Application.py
+++ Application.py
@@ -15,11 +15,22 @@ class Application(QtWidgets.QMainWindow):
         pass
 
     def open_file_click(self):
-        self.ui.data_type.isEnabled = False
         file_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Открытие файла', '/home', '*.json')[0]
-        with open(file_name, 'r') as fl:
-            data = json.load(fl)
-            keys = list(data[0].keys())
+        try:
+            with open(file_name, 'r') as fl:
+                data = json.load(fl)
+                self.setWindowTitle("JSON GUI - " + file_name)
+                self.ui.data_type.setEnabled(False)
+                self.ui.tree.clear()
+                self.ui.table.setRowCount(0)
+                self.ui.table.setColumnCount(0)
+        except FileNotFoundError:
+            pass
+        except FileExistsError:
+            QtWidgets.QMessageBox(self, "Такого файла не существует", QtWidgets.QMessageBox.StandardButton.Ok)
+
+        try:
+            keys = list(data[0].keys())        
             self.db.append(keys)
             data_array = []
             for x in data:
@@ -44,21 +55,30 @@ class Application(QtWidgets.QMainWindow):
                     item.setText(1, "boolean")
                 elif isinstance(data_array[0][i], None):
                     item.setText(1, "null")
-            
+                
             self.ui.table.setColumnCount(len(keys))
             self.ui.table.setRowCount(len(data_array))
             self.ui.table.setHorizontalHeaderLabels(keys)
-            row = 0
+            rows = 0
             for item in data_array:
                 for i in range(len(data_array)):
                     for j in range(len(keys)):
-                        self.ui.table.setItem(row, j, QtWidgets.QTableWidgetItem(str(data_array[i][j])))
-                    row += 1
+                        self.ui.table.setItem(rows, j, QtWidgets.QTableWidgetItem(str(data_array[i][j])))
+                    rows += 1
+        except IndexError:
+            pass
+        except UnboundLocalError:
+            pass
 
-            self.ui.table.cellClicked.connect(self.current_cell)
+        self.ui.table.cellClicked.connect(self.current_cell)
+        self.ui.table.selectionModel().clearCurrentIndex()
 
     def current_cell(self):
-        self.ui.cell_editing.setText(self.ui.table.currentItem().text())
+        try:
+            self.ui.cell_editing.setText(self.ui.table.currentItem().text())
+            self.ui.key_editing.setText(self.db[0][self.ui.table.currentColumn()])
+        except AttributeError:
+            pass
 
     def save_file_click(self):
         pass
@@ -81,19 +101,40 @@ class Application(QtWidgets.QMainWindow):
         self.about_program.show()
 
     def add_entry_button_click(self):
-        pass
+        self.ui.table.insertRow(self.ui.table.rowCount())
+        for i in range(self.ui.table.columnCount()):       
+            self.ui.table.setItem(self.ui.table.rowCount() - 1, i, QtWidgets.QTableWidgetItem("null"))
 
     def add_col_button_click(self):
-        pass
+        self.ui.table.insertColumn(self.ui.table.columnCount())
+        self.db[0].append(str(self.ui.table.columnCount()))
+        for i in range(self.ui.table.rowCount()):       
+            self.ui.table.setItem(i, self.ui.table.columnCount() - 1, QtWidgets.QTableWidgetItem("null"))
 
     def delete_entry_button_click(self):
-        pass
+        if self.ui.table.currentRow() > -1:
+            self.ui.table.removeRow(self.ui.table.currentRow())
+            self.ui.table.selectionModel().clearCurrentIndex()
+            self.ui.cell_editing.setText(None)
+            self.ui.key_editing.setText(None)
 
     def delete_col_button_click(self):
-        pass
+        if self.ui.table.currentColumn() > -1:
+            self.db[0].remove(self.db[0][self.ui.table.currentColumn()])
+            self.ui.table.removeColumn(self.ui.table.currentColumn())
+            self.ui.table.selectionModel().clearCurrentIndex()
+            self.ui.cell_editing.setText(None)
+            self.ui.key_editing.setText(None)
 
     def apply_button_click(self):
-        self.ui.table.currentItem().setText(self.ui.cell_editing.toPlainText())
+        try:
+            self.ui.table.currentItem().setText(self.ui.cell_editing.toPlainText())
+        except AttributeError:
+            pass
+
+    def apply_key_button_click(self):
+        self.db[0][self.ui.table.currentColumn()] = self.ui.key_editing.text()
+        self.ui.table.setHorizontalHeaderLabels(self.db[0])
 
     def goto_button_click(self):
         pass
